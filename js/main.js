4c1b401b4cbb77c8866222e4126df8a903659166
chenmo <chenmo1212@qq.com>
2021-04-28 22:48:11 +0800

    lazyLoad img

--- js/main.js
+++ js/main.js
@@ -595,10 +595,48 @@ if (darkMode === "enabled") {
 
 // Listeners
 const darkModeToggle = document.querySelector(".mode");
-darkModeToggle.addEventListener("click", () => {
-	if (darkMode === 'enabled'){
-		disableDarkMode();
-	} else {
-		enableDarkMode();
-	}
-});
+darkModeToggle.click(function () {
+    if (darkMode === 'enabled'){
+        disableDarkMode();
+    } else {
+        enableDarkMode();
+    }
+})
+
+/**
+ * 图片懒加载
+ */
+var num = document.getElementsByTagName('img').length;
+var img = document.getElementsByTagName("img");
+// 存储图片加载到的位置，避免每次都从第一张图片开始遍历
+var n = 0;
+// 页面载入完毕加载可视区域内的图片
+lazyLoad();
+window.onscroll = lazyLoad;
+// 监听页面滚动事件
+function lazyLoad() {
+    console.log(n)
+// 可见区域高度
+    var seeHeight = document.documentElement.clientHeight;
+// 滚动条距离顶部高度
+    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    for (let i = n; i < num; i++) {
+
+        // 用于定位的图片(offset仅为相对父元素的距离，因此需要进行判断）
+        let offsetTop = img[i].offsetTop;
+        let par = img[i].offsetParent;
+        if (par.nodeName.toLowerCase() !== 'body'){
+            while (par){
+                offsetTop += par.offsetTop;
+                par = par.offsetParent;
+            }
+        }
+        if (offsetTop < seeHeight + scrollTop + 300) {  // 提前缓冲
+            console.log(i, '----', offsetTop)
+            if (img[i].getAttribute("src") === "https://gitee.com/chenmo1212/img/raw/master/1.jpg") {
+                img[i].src = img[i].getAttribute("data-src");
+            }
+            n = i + 1;
+        }
+    }
+}
